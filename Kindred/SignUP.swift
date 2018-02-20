d55120229ed8fbc8a30acdc31a2ed80e3696ec6b
Scott Dalym <rscottdaly@gmail.com>
2018-02-19 20:31:49 -0700

    Perform initial steps of facebook login integration

---
+++ Kindred/SignUP.swift
@@ -0,0 +1,22 @@
+//
+//  SignUP.swift
+//  Kindred
+//
+//  Created by Administrator on 2/19/18.
+//  Copyright © 2018 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class SignUP: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+ 
+
+
+}
